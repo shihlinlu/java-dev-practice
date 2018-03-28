3c9ac22d27946fac95fcda963b3d020df85fa5c5
Shihlin Lu <shihlinlu@gmail.com>
2018-03-28 17:38:38 -0600

    Review of data types, basic math operations, concatenations, casting, etc

---
+++ src/module_1/MathOperations.java
@@ -0,0 +1,22 @@
+package module_1;
+
+public class MathOperations {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        System.out.println("x + y = " + (x + y));
+        System.out.println("x - y = " + (x - y));
+        System.out.println("x * y = " + (x * y));
+        System.out.println("x / y = " + (x / y));
+        Second();
+    }
+
+    public static void Second() {
+        double x = 10;
+        double y = 3;
+        System.out.println("x + y = " + (x + y));
+        System.out.println("x - y = " + (x - y));
+        System.out.println("x * y = " + (x * y));
+        System.out.println("x / y = " + (x / y));
+    }
+}
